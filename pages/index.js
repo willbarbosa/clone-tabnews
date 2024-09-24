62043fd53a44ddf37195a26c420184d5b229889b
William R. Barbosa <37556105+willbarbosa@users.noreply.github.com>
2024-09-24 02:10:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Teste, essa alteração está sendo feita após o primeiro deploy na vercel!</h1>
+  return (
+    <h1>
+      Teste, essa alteração está sendo feita após o primeiro deploy na vercel!
+    </h1>
+  );
 }
 
 export default Home;
