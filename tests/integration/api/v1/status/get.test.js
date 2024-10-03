8b36618c5934cd4efdf95592b79d4a727aa7d417
William R. Barbosa <37556105+willbarbosa@users.noreply.github.com>
2024-10-03 11:44:58 +0000

    remove `console.log()`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,7 +6,6 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  console.log(responseBody);
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opended_connections).toEqual(1);
